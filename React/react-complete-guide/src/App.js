61f4224cdb2dfcd7c602742726ce229578d5f77c
lyndoncortez <lyndoncortez@yahoo.com>
2021-01-22 12:37:03 +0800

    add react-bootstrap

--- React/react-complete-guide/src/App.js
+++ React/react-complete-guide/src/App.js
@@ -1,10 +1,48 @@
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 import './App.css';
-import Person from './Person';
 
 function App() {
+  const[user, setUser] = useState({
+    fName: "",
+    lName: "",
+  });
+
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+
+    setUser((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        }
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        }
+      }
+    })
+  }
+
+
   return (
     <div className="App">
-      <Person name="Manu" age="24" />
+    <Container>
+      <Form >
+            <Form.Row className="justify-content-md-center" >
+                <Col sm={12} md={6} >
+                    <Form.Control placeholder="First name" name="fName" value={user.fName} onChange={handleChange} />
+                </Col>
+                <Col sm={12} md={6} className="mt-3 mt-sm-3 mt-md-0">
+                    <Form.Control placeholder="Last name" name="lName" value={user.lName} onChange={handleChange} />
+                </Col>
+            </Form.Row>
+        </Form>
+      </Container>
     </div>
   );
 }
